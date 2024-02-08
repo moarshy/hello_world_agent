026b2ab0045aef229864e9c40bc7805e110e2576
moarshy <marshath@gmail.com>
2024-02-08 15:33:09 +0800

    fixes

--- hello_world_daimon_template/run.py
+++ hello_world_daimon_template/run.py
@@ -1,17 +1,17 @@
-from hello_world_daimon_template.schema import JobRequestSchema
+from hello_world_daimon_template.schema import InputSchema
 from hello_world_daimon_template.utils import get_logger
 
 
 logger = get_logger(__name__)
 
 
-def run(job: JobRequestSchema):
+def run(job: InputSchema):
     logger.info(f"Running job {job.name}")
     return job.param1 + job.param2
 
 
 if __name__ == "__main__":
-    job = JobRequestSchema(
+    job = InputSchema(
         name="addition",
         description="Add two numbers",
         param1=1,
