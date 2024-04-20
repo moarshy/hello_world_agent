96961038157c5c0e6274a42f865ccb18bc302460
Richard Blythman <richardblythman@gmail.com>
2024-04-20 11:02:53 +0100

    change from add to print

--- hello_world/run.py
+++ hello_world/run.py
@@ -1,21 +1,18 @@
+#!/usr/bin/env python
+
 from hello_world.schemas import InputSchema
 from hello_world.utils import get_logger
 
-
 logger = get_logger(__name__)
 
-
-def run(job: InputSchema, cfg: dict = None, **kwargs):
-    logger.info(f"Running job {job.name}")
+def run(inputs: InputSchema, cfg: dict = None, **kwargs):
+    logger.info(f"Running job with inputs {inputs}")
     logger.info(f"cfg: {cfg}")
-    return job.param1 + job.param2
-
+    return f"Hello {inputs.param1} + {inputs.param2}"
 
 if __name__ == "__main__":
-    job = InputSchema(
-        name="addition",
-        description="Add two numbers",
-        param1=1,
-        param2=2,
+    inputs = InputSchema(
+        param1="world",
+        param2="naptha",
     )
-    print(run(job))
+    print(run(inputs))
--- hello_world/schemas.py
+++ hello_world/schemas.py
@@ -2,7 +2,5 @@ from pydantic import BaseModel
 
 
 class InputSchema(BaseModel):
-    name: str
-    description: str
-    param1: int
-    param2: int
+    param1: str
+    param2: str
