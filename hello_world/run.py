f6a691451a8f1382c233062634375bae0e009965
Richard Blythman <richardblythman@gmail.com>
2024-06-04 13:04:35 +0100

    new input parameters

--- hello_world/run.py
+++ hello_world/run.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 from hello_world.schemas import InputSchema
-from hello_world.utils import get_logger
+from naptha_sdk.utils import get_logger
 
 logger = get_logger(__name__)
 
-def run(inputs: InputSchema, cfg: dict = None, **kwargs):
-    logger.info(f"Running job with inputs {inputs}")
+def run(inputs: InputSchema, worker_nodes = None, orchestrator_node = None, flow_run = None, cfg: dict = None, **kwargs):
+    logger.info(f"Running module with inputs {inputs}")
     logger.info(f"cfg: {cfg}")
     return f"Hello {inputs.param1} + {inputs.param2}"
 
