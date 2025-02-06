e8bb5990b5e885264f6e94738b6190747e5fe19a
moarshy <marshath@gmail.com>
2025-02-06 10:49:10 +0800

    v0.2

--- hello_world_agent/run.py
+++ hello_world_agent/run.py
@@ -14,7 +14,7 @@ def run(module_run: Dict, *args, **kwargs):
     logger.info(f"SERPER_API_KEY: {os.getenv('SERPER_API_KEY')}")
     module_run = AgentRunInput(**module_run)
     module_run.inputs = InputSchema(**module_run.inputs)
-    return f"Hello {module_run.inputs.firstname} {module_run.inputs.surname} from v0.1. Numpy version: {np.__version__}"
+    return f"Hello {module_run.inputs.firstname} {module_run.inputs.surname} from v0.2. Numpy version: {np.__version__}"
 
 if __name__ == "__main__":
     import asyncio
